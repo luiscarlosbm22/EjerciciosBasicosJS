391e22294064a7dd88c590db051e52fef304b4ce
Luis Bautista Machuca <luiscarlosbautistamachuca@gmail.com>
2022-07-19 01:06:41 -0500

    agregando js al ejercicios 2

--- funciones.js
+++ funciones.js
@@ -28,3 +28,50 @@ function calcularParesImpares(){
     }
 
 }
+function calcularPromedioNegPos(){
+    let contNeg=0, cantPos=0, term=0;
+    let promPos, promNeg=0;
+    let Numero=0;
+    let numI=0; 
+
+    let romp= new Boolean (true);
+    Numero = document.getElementById('N_numero').value;
+
+
+    while(romp){
+
+        for(let i=1; 1<=Numero; i++){
+            numI = parseFloat(prompt("Ingrese Nmumero"));
+            if(numI>0){
+                cantPos++;
+                promPos=promPos+numI;
+            }
+            if(numI>0){
+                cantNeg++;
+                promNeg=promNeg+numI;
+            }
+        }
+        if(cantPos==0){
+            promPos=0;
+        }else{
+            promPos=promPos/cantPos;
+        }
+        if(cantNeg==0){
+            promNeg=0;
+        }else{
+            promNeg=promNeg/cantNeg;
+        }
+            if(term<=0){
+                romp=false;
+            }else{
+                console.log(" ");
+            }
+
+
+    }
+    document.getElementById('promPos').value=promPos;
+    console-log(promPos);
+    document.getElementById('promNeg').value=promBeg;
+    console.log(promNeg);
+
+}
